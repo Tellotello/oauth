33a75939ebe86a4e208a049d9e0a4d73858a77d1
Tello <95242465+Tellotello@users.noreply.github.com>
2024-05-26 23:14:51 -0600

    Add files via upload

---
+++ OAuth client/models/clients.js
@@ -0,0 +1,10 @@
+const clients = [
+  {
+    id: "1",
+    clientId: "client1",
+    clientSecret: "secret1",
+    redirectUris: ["https://oauthtello.azurewebsites.net/callback"], // Correct URL
+  },
+];
+
+module.exports = { clients };
